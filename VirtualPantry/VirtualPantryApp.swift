6fee0c22ec47fa37eb3302b4b7794297bac4a15b
James Dansie <james@dansie.com>
2022-11-03 11:09:25 -0600

    Initial Commit

---
+++ VirtualPantry/VirtualPantryApp.swift
@@ -0,0 +1,17 @@
+//
+//  VirtualPantryApp.swift
+//  VirtualPantry
+//
+//  Created by James Dansie on 11/3/22.
+//
+
+import SwiftUI
+
+@main
+struct VirtualPantryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
